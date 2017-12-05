63b638619d24720999e4928241c1bf1866874662
Anthony Bracewell <anthonybracewell.violinist@gmail.com>
2017-12-05 01:47:34 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if (index <= 0 && index >= 8) && (position_taken? == false)
+  if (index <= 0 && index >= 8) #&& (position_taken? == false)
     true
   else
     false
